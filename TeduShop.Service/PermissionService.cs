c8b9788402eea2ed520ace281dcae291fc650cc1
anhtrong211998 <anhtrong211998@gmail.com>
2021-02-01 09:19:59 +0700

    Add project files.

---
+++ TeduShop.Service/PermissionService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IPermissionService
+    {
+        ICollection<Permission> GetByFunctionId(string functionId);
+        ICollection<Permission> GetByUserId(string userId);
+        void Add(Permission permission);
+        void DeleteAll(string functionId);
+        void SaveChange();
+    }
+
+    public class PermissionService : IPermissionService
+    {
+        private IPermissionRepository _permissionRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public PermissionService(IPermissionRepository permissionRepository, IUnitOfWork unitOfWork)
+        {
+            this._permissionRepository = permissionRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(Permission permission)
+        {
+            _permissionRepository.Add(permission);
+        }
+
+        public void DeleteAll(string functionId)
+        {
+            _permissionRepository.DeleteMulti(x => x.FunctionId == functionId);
+        }
+
+        public ICollection<Permission> GetByFunctionId(string functionId)
+        {
+            return _permissionRepository
+                .GetMulti(x => x.FunctionId == functionId, new string[] { "AppRole", "AppRole" })
+                .Where(m => m.AppRole.Name != "Admin").ToList();
+        }
+
+        public ICollection<Permission> GetByUserId(string userId)
+        {
+            return _permissionRepository.GetByUserId(userId);
+        }
+
+        public void SaveChange()
+        {
+            _unitOfWork.Commit();
+        }
+    }
+}
